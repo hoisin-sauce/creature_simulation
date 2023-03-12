b83a8fe1e8df13e8d62b590b8e4a03f034dcbd0e
hoisin-sauce <97987829+hoisin-sauce@users.noreply.github.com>
2023-03-12 13:52:21 +0000

    adding numbers

--- neuralnet.py
+++ neuralnet.py
@@ -1,7 +1,7 @@
 import math
 import random
 from collections import deque
-import copy
+import heapq
 
 
 def chunks(l, n):
@@ -63,8 +63,8 @@ class Node:
         if bias_weight is not None:
             self.bias_weight = bias_weight
 
-    def mutate_node(self, weight_chance, weight_range):
-        for i in range(self.node_weights):
+    def mutate(self, weight_chance, weight_range):
+        for i in range(len(self.node_weights)):
             if random.uniform(0, 1) > weight_chance:
                 self.node_weights[i] += random.uniform(*weight_range)
 
@@ -83,6 +83,9 @@ class Layer:
         if default_weights is None:
             for node in self.nodes:
                 node.set_weights(Weights.generate_random_weights(output_layer))
+        else:
+            for node, weights in zip(self.nodes, default_weights):
+                node.set_weights(weights[1:], bias_weight=weights[0])
 
     def run_layer(self, weight_inputs: list):
         if self.is_output:
@@ -101,15 +104,29 @@ class Layer:
         for node in self.nodes:
             node.mutate(weight_chance, weight_range)
 
+    def get_weights(self):
+        return [[node.bias_weight, *node.node_weights] for node in self.nodes]
+
 
 class Network:
-    def __init__(self, shape: list, default_weights=None):
+    def __init__(self, _shape: list, default_weights=None):
         self.layers = list()
+        self.shape = _shape[:]
+        shape = _shape[:]
         shape.reverse()
-        self.layers.append(Layer(None, shape[0], default_weights=default_weights, is_output=True))
-        shape.pop(0)
-        for i in shape:
-            self.layers.append(Layer(self.layers[-1], i))
+        if default_weights is not None:
+            d_w = None if len(default_weights) < 1 else default_weights[0]
+            self.layers.append(Layer(None, shape[0], default_weights=d_w, is_output=True))
+            shape.pop(0)
+            default_weights.pop(0)
+            for i, weights in zip(shape, default_weights):
+                self.layers.append(Layer(self.layers[-1], i, default_weights=weights))
+        else:
+            self.layers.append(Layer(None, shape[0], default_weights=None, is_output=True))
+            shape.pop(0)
+            for i in shape:
+                self.layers.append(Layer(self.layers[-1], i))
+        self.fitness = 0
 
     def run_network(self, inputs):
         return self.layers[-1].run_layer(inputs)
@@ -118,7 +135,62 @@ class Network:
         for layer in self.layers:
             layer.mutate_layer(weight_chance, weight_range)
 
+    def duplicate_network(self):
+        weights = [layer.get_weights() for layer in self.layers]
+        return Network(self.shape, default_weights=weights)
+
 
 if __name__ == "__main__":
-    example_network = Network([1, 4, 4, 2])
-    print(example_network.run_network((1,)))
+    # test_data
+    test_data = [[a, b, a + b] for a in range(-100, 100) for b in range(-100, 100)]
+
+    def fitness(network: Network):
+        # print(network.run_network([1, 1])[0])
+        return sum([
+            math.sqrt(
+                (test[2] - network.run_network([test[0], test[1]])[0])**2)
+            for test in test_data]) / len(test_data)
+
+    # generate 99 networks
+    # run all networks to get fitness
+    # delete lower 66 networks
+    # create 2 clones of each of remaining 33
+    # mutate clones
+    # repeat untill average fitness < 0.01
+
+    networks = [Network([2, 4, 4, 4, 4, 4, 1]) for _ in range(99)]
+    mean_fitness = 100
+    inc = 0
+    while mean_fitness > 0.1:
+        total_fit = 0
+        sorted_networks = []
+        for i, network in enumerate(networks):
+            print(i)
+            network.fitness = fitness(network)
+            heapq.heappush(sorted_networks, (network.fitness, network))
+            total_fit += network.fitness
+        mean_fitness = total_fit / len(networks)
+
+        surviving_networks = []
+        while sorted_networks:
+            surviving_networks.append(heapq.heappop(sorted_networks)[1])
+        networks = surviving_networks[:33]
+
+
+        additional_clones = list()
+
+        for network in networks:
+            net1, net2 = [network.duplicate_network() for _ in range(2)]
+            net1.mutate_network(0.1, [-0.02, 0.02])
+            net2.mutate_network(0.1, [-0.02, 0.02])
+            additional_clones.append(net1)
+            additional_clones.append(net2)
+
+        networks.extend(additional_clones)
+        inc += 1
+        print(f"{inc} : {mean_fitness}")
+
+    [print(networks[i].run_network([1, 1])) for i in range(99)]
+    print(len(additional_clones))
+    print(inc)
+
